7220ab445ec3730b8b521776398aaecea86cf714
Carolina Semerano <carol_semerano@live.com>
2022-06-03 18:51:18 -0400

    adding new layers

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -32,11 +32,20 @@ let baseMaps = {
 let allEarthquakes = new L.LayerGroup();
 let tectonicPlates = new L.LayerGroup();
 
+// Add a 3rd layer group for the major earthquake data.
+let majorEarthquakes = new L.LayerGroup();
+
+// Add a 3rd layer group for the past hour earthquake data.
+let pastHourEarthquakes = new L.LayerGroup();
+
 
 // 2. Add a reference to the tectonic plates group to the overlays object.
 let overlays = {
   "Earthquakes": allEarthquakes,
-  "Tectonic Plates": tectonicPlates
+  "Tectonic Plates": tectonicPlates,
+  // Add a reference to the major earthquake group to the overlays object.
+  "Major Earthquakes": majorEarthquakes,
+  "Past Hour Earthquakes": pastHourEarthquakes
 };
 
 // Then we add a control to the map that will allow the user to change which
@@ -106,10 +115,78 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     }
   }).addTo(allEarthquakes);
 
-  // Then we add the earthquake layer to our map.
-  allEarthquakes.addTo(map);
+ // Then we add the earthquake layer to our map.
+allEarthquakes.addTo(map);
 
-  // Here we create a legend control object.
+// Retrieve the past hour earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson").then(function(data) {
+
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJson(data, {
+     // We create a popup for each Marker to display the magnitude and location of the earthquake
+     onEachFeature: function(feature, layer) {
+      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+    }
+  }).addTo(pastHourEarthquakes);
+
+ // Then we add the earthquake layer to our map.
+ pastHourEarthquakes.addTo(map);
+});
+
+// 3. Retrieve the major earthquake GeoJSON data > 4.5 mag for the week.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson").then(function(data) {
+
+  // 4. Use the same style as the earthquake data.
+  function styleInfo(feature) {
+    return {
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: getColor(feature.properties.mag),
+      color: "#000000",
+      radius: getRadius(feature.properties.mag),
+      stroke: true,
+      weight: 0.5
+    };
+  };
+  
+  // 5. Change the color function to use three colors for the major earthquakes based on the magnitude of the earthquake.
+  function getColor(magnitude) {
+    if (magnitude > 6) {
+      return "#ea2c2c";
+    }
+    if (magnitude > 5) {
+      return "#ea822c";
+    }
+    return "#eecc00";
+  };
+  
+  // 6. Use the function that determines the radius of the earthquake marker based on its magnitude.
+  function getRadius(magnitude) {
+    if (magnitude === 0) {
+      return 1;
+    }
+    return magnitude * 4;
+  };
+  
+  // 7. Creating a GeoJSON layer with the retrieved data that adds a circle to the map 
+  // sets the style of the circle, and displays the magnitude and location of the earthquake
+  //  after the marker has been created and styled.
+  L.geoJson(data, {
+    pointToLayer: function(feature, latlng) {
+      console.log(data);
+      return L.circleMarker(latlng);
+    },
+    style: styleInfo,
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+    }
+  }).addTo(majorEarthquakes);
+  // 8. Add the major earthquakes layer to the map.
+  majorEarthquakes.addTo(map);
+  // 9. Close the braces and parentheses for the major earthquake data.
+});
+
+// Here we create a legend control object.
 let legend = L.control({
   position: "bottomright"
 });
@@ -144,7 +221,7 @@ legend.onAdd = function() {
   // Create a style for the lines.
   let myStyle = {
     color: "Red",
-    weight: 1
+    weight: 2
   };
 
   // 3. Use d3.json to make a call to get our Tectonic Plate geoJSON data.
